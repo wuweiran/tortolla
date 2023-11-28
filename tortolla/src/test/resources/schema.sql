f5e9740dfe733e836e9faa73d23adb2b0417d0fc
Weiran Wu <weiranwu@microsoft.com>
2023-11-28 10:34:59 +0800

    Make back end runnable in test scope

---
+++ tortolla/src/test/resources/schema.sql
@@ -0,0 +1,22 @@
+CREATE SCHEMA `tortolla` DEFAULT CHARACTER SET utf8mb4;
+USE tortolla;
+
+CREATE TABLE `user` (
+	id BIGINT(20) not null auto_increment,
+	username VARCHAR(100) not null,
+	password_hash VARCHAR(200) not null,
+	full_name VARCHAR(200),
+	created_time TIMESTAMP not null,
+	primary key(id),
+	unique key(username)
+);
+
+CREATE TABLE post (
+	id BIGINT(30) not null auto_increment,
+	title VARCHAR(100),
+	body TEXT,
+	author_id BIGINT(20) not null,
+	created_time TIMESTAMP not null,
+	last_updated_time TIMESTAMP not null,
+	primary key(id)
+);
