68d076d48f8ac4352f31fcd73dea7287942de6fc
wrsuperboy <wwr42@126.com>
2023-12-15 20:08:12 +0800

    Update back-end endpoint

--- tortolla-web/src/containers/api.ts
+++ tortolla-web/src/containers/api.ts
@@ -2,7 +2,7 @@ import { isSignedIn, loadCurrentUserToken, signOut } from "./user.ts";
 
 const endpoint = import.meta.env.DEV
   ? "http://localhost:8080"
-  : "https://tortolla-service-tortolla.azuremicroservices.io";
+  : "https://tortolla.whiteisland-82d21b46.eastasia.azurecontainerapps.io";
 
 enum ApiResponseStatus {
   SUCCESS = 0,
